f205f3a71c13434f55e743ea5709e0d36d68bdfc
Gustaf Andersson <gustaf@icloud.com>
2016-03-01 22:45:04 +0100

    Optimized inner loop iteration

--- levenshtein-sse.hpp
+++ levenshtein-sse.hpp
@@ -191,12 +191,13 @@ struct LevenshteinIterationBase {
 static inline void perform(const Iterator1& a, const Iterator2& b,
   std::size_t& i, std::size_t j, std::size_t bLen, Vec1& diag, const Vec2& diag2)
 {
-  int substitutionCost = a[i-1] == b[j-1] ? -1 : 0;
-  diag[i] = std::min({
-    diag2[i-1],
-    diag2[i],
-    diag[i-1] + substitutionCost
-  }) + 1;
+  std::uint32_t min = std::min(diag2[i], diag2[i-1]);
+  if (min < diag[i-1]) {
+    diag[i] = min + 1;
+  }
+  else {
+    diag[i] = diag[i-1] + (a[i-1] != b[j-1]);
+  }
   --i;
 }
 };
